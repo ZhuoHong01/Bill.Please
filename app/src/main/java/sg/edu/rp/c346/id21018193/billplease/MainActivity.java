57cda60c1e240a665593f11c268ef2bbe1d74d47
lee zhuo hong <21018193@myrp.edu.sg>
2022-05-04 14:07:04 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id21018193/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/billplease/MainActivity.java
@@ -2,12 +2,27 @@ package sg.edu.rp.c346.id21018193.billplease;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText et;
+    TextView tv;
+    ToggleButton tbtn;
+    RadioGroup rgroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
